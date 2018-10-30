6dc5c1ecd66e47a0af67d17287ff88cd7bde8beb
Cruz <Cruz@DESKTOP-4DSEQUK>
2018-10-30 14:18:15 -0700

    Changed 'skeleton' branch to 'A4_Implementation'

--- js/project.js
+++ js/project.js
@@ -1,3 +1,11 @@
 function showSideMenu() {
-  
+  document.getElementById("sideMenu").style.width="400px";
+}
+
+function hideSideMenu() {
+  document.getElementById("sideMenu").style.width="0px";
+}
+
+function rotateArrow() {
+
 }
